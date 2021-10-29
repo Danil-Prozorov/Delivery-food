7108e0d09702e19f2e98eca0947f8ea405c24e60
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-10-29 19:30:03 +0300

    Created model in modules 'Restaurents'

---
+++ app/Modules/Pub/Restaurants/Models/Restaurant_products.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Pub\Restaurants\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant_products extends Model
+{
+    use HasFactory;
+}
