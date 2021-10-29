531ff22a732d1e4a4885650c851d7b4178d1d3d1
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-10-29 19:28:35 +0300

    Created modul for cart

---
+++ app/Modules/Pub/Cart/Migrations/2021_10_29_145941_create_cart_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id')->required();
+            $table->string('product_name')->required();
+            $table->integer('product_count')->default(1);
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
