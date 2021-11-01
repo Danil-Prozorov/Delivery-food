9dc0334eba9012277ef4029d90315ecce0cc0a8b
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-11-01 19:14:06 +0300

    Created a new module for giving and show user score

---
+++ app/Modules/Pub/Feedback/Models/Feedback.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Pub\Feedback\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    use HasFactory;
+}
