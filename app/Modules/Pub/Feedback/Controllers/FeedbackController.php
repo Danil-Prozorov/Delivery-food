ddacab8fc7f458d238db6c14d47d02078f5077a8
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-11-10 16:46:35 +0300

    Created controller for feedback

---
+++ app/Modules/Pub/Feedback/Controllers/FeedbackController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Modules\Pub\Feedback\Controllers;
+
+use App\Modules\Pub\Feedback\Models\Feedback;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FeedbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Create of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Modules\Pub\Feedback\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Modules\Pub\Feedback\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Modules\Pub\Feedback\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Modules\Pub\Feedback\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Feedback $feedback)
+    {
+        //
+    }
+}
