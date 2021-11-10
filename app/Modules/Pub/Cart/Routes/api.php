3b2152b68c1810dfddbd3c368faa637d0bb2a39f
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-11-10 16:45:01 +0300

    Deleted useles api files

--- app/Modules/Pub/Cart/Routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'carts', 'middleware' => []], function () {
-    Route::get('/', 'Api\CartController@index')->name('api.carts.index');
-    Route::post('/', 'Api\CartController@store')->name('api.carts.store');
-    Route::put('/{cart}', 'Api\CartController@update')->name('api.carts.update');
-    Route::delete('/{cart}', 'Api\CartController@destroy')->name('api.carts.delete');
-});
