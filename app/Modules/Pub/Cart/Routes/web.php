6f95d2b05e316ca0855dffc834e6c9abaed31848
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-11-20 19:30:00 +0300

    Updated web.php.Added route update

--- app/Modules/Pub/Cart/Routes/web.php
+++ app/Modules/Pub/Cart/Routes/web.php
@@ -2,4 +2,6 @@
 
 Route::group(['prefix' => 'cart', 'middleware' => []], function () {
     Route::get('/', 'CartController@index')->name('cart.index');
+    Route::post('/', 'CartController@store')->name('cart.store');
+    Route::patch('/update/{id}','CartController@update')->name('cart.update');
 });
