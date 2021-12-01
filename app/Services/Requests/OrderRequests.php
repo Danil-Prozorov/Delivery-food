84a7e80d5992953841eef0f3b2b4377cff4e1fcc
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-12-01 21:24:41 +0300

    Created service for making orders

---
+++ app/Services/Requests/OrderRequests.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Requests;
+
+use App\Modules\Pub\Restaurants\Models\Orders;
+use App\Modules\Admin\User\Models\User;
+use App\Modules\Pub\Cart\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+use App\Services\Response\ResponseService as Response;
+
+class OrderRequests
+{
+  
+}
