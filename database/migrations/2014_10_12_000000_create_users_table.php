0df488eb8f1d6fee108a707d5b57c1fefae364a3
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-12-28 20:26:53 +0300

    Added DB for testing in PHPUnit with SQLite

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('api_token', 60)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('api_token', 60)->unique()->nullable();
             $table->timestamps();
         });
     }
