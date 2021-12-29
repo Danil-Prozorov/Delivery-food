8d0641153346a155e06cdc029659e844dab42e5c
Danil-Prozorov <danil.prozoroff2018@yandex.ru>
2021-12-29 15:32:37 +0300

    Added few methods for get info about Orders

--- app/Services/Requests/OrderRequests.php
+++ app/Services/Requests/OrderRequests.php
@@ -26,7 +26,7 @@ class OrderRequests implements OrderRequestInterface
       $dataOrder = array(
           'user_id'     => Auth::id(),
           'total_price' => $request['total_price'],
-          'adres'       => $request['adres'],
+          'address'     => $request['address'],
           'status'      => "In proccess",
       );
 
@@ -38,7 +38,7 @@ class OrderRequests implements OrderRequestInterface
       self::createOrderDetails($this->cart, array(), $orderCreated);
       self::cleanCart($this->cart,array());
 
-      return self::success();
+      return self::success(['order_id' => $orderCreated]);
   }
 
   protected function createOrderDetails($array, $results, $orderId)
@@ -63,28 +63,76 @@ class OrderRequests implements OrderRequestInterface
 
   protected function cleanCart($array,$result)
   {
-    $counterResult = count($result);
-    if (count($array) == $counterResult) {
+      // counter
+      $counterResult = count($result);
+      // if we delete all items then just do nothing
+      if (count($array) == $counterResult) {
 
-    } elseif (count($array) > $counterResult) {
-        $array[$counterResult]->delete();
+      } elseif (count($array) > $counterResult) { // else delete item and call method again
+          $array[$counterResult]->delete();
 
-        array_push($result,0);
-        return $this->cleanCart($array,$result);
-    }
+          array_push($result,0);
+          return $this->cleanCart($array,$result);
+      }
   }
 
   public function cancelOrder($orderID)
   {
-
+      // Find order
+      $order = Orders::find($orderID)->first();
+      // Validate user_id and id for authorized right now
+      if (!($order['user_id'] == Auth::id())) {
+          return self::badRequest(['error' => 'Incorrect ID for this session']);
+      }
+      // Checking order status
+      if ($order['status'] == 'Delivered' || $order['status'] == 'Canceled') {
+         return self::noContent();
+      }
+      // Update order status to 'Canceled'
+      $order->update(['status' => 'Canceled']);
+      // return
+      return self::resetContent();
   }
 
   public function completeOrder($orderID)
   {
       // Find the order with the same orderID
-      $order = Order::where('id','=',$orderID)->get();
-      if()
-      return self::success();
+      $order = Orders::where('id','=',$orderID)->get();
+
+      // Validate ID
+      if (!($order['user_id'] == Auth::id())) {
+          return self::badRequest(['error' => 'Incorrect ID for this session']);
+      }
+
+      // Update order status
+      $order->update(['status' => 'Delivered']);
+
+      // return
+      return self::resetContent();
+  }
+
+  public function getAllOrders($userID)
+  {
+      // Checking userID
+      if (!($userID == Auth::id())) {
+          return self::badRequest(['error' => 'Incorrect ID for this session']);
+      }
+      // If ID the same then...
+      $orders = Orders::all()->where('user_id', '=', $userID);
+      return $orders;
+  }
+
+  public function getOrder($userID, $orderID)
+  {
+      // Validate userID
+      if (!($userID == Auth::id())) {
+         return self::badRequest(['error' => 'Incorrect ID for this session']);
+      }
+      // Find order
+      $order = Orders::find($orderID)->first();
+      // return order
+      return $order;
+
   }
 
   public function getOrderDetails($orderID) // get userID and orderID
@@ -92,12 +140,21 @@ class OrderRequests implements OrderRequestInterface
       //get data form DB
       $orderData = Orders_details::where('order_id', '=', $orderID)->get();
 
+      // check authentication
+      if (!Auth::user()) {
+        self::noContent();
+      }
+
       //return it
       return $orderData;
   }
 
   public function getAllOrderDetails($userID)
   {
+      // Get data from DB
+      $orderData = Orders_details::where('user_id', '=', $userID)->get();
 
+      // return result
+      return $orderData;
   }
 }
